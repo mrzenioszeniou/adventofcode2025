402903b54cabb76b06f831daddc7064eba900e9c
Zenios Zeniou <mrzenioszeniou@gmail.com>
2025-12-02 19:39:04 +0100

    Day 1

---
+++ day1.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Day1 struct {
+	rotations []rotation
+}
+
+type direction int
+
+const (
+	left direction = iota
+	right
+)
+
+type rotation struct {
+	amount    int64
+	direction direction
+}
+
+func parseRotation(s string) (rotation, error) {
+	rot := rotation{}
+	switch s[0] {
+	case 'L':
+		rot.direction = left
+	case 'R':
+		rot.direction = right
+	default:
+		return rotation{}, errors.New("invalid direction")
+	}
+
+	amount, err := strconv.ParseInt(s[1:], 10, 64)
+	if err != nil {
+		return rotation{}, err
+	}
+
+	rot.amount = amount
+
+	return rot, nil
+}
+
+func (d *Day1) Parse(input string) error {
+	var rots []rotation
+
+	for line := range strings.SplitSeq(input, "\n") {
+		if rot, err := parseRotation(line); err == nil {
+			rots = append(rots, rot)
+		} else {
+			return fmt.Errorf("invalid rotation %q encountered: %w", line, err)
+		}
+	}
+
+	d.rotations = rots
+
+	return nil
+}
+
+func (d *Day1) Solve() (part1 string, part2 string, err error) {
+	var p1 int64
+	var p2 int64
+	var curr int64 = 50
+	for _, rot := range d.rotations {
+		prev := curr
+		p2 += rot.amount / 100
+		amount := rot.amount % 100
+		switch rot.direction {
+		case left:
+			curr -= amount
+		case right:
+			curr += amount
+		}
+
+		if curr == 0 {
+			p1++
+			p2++
+		} else if curr < 0 {
+			curr += 100
+			if prev != 0 {
+				p2++
+			}
+		} else if curr >= 100 {
+			curr -= 100
+			p2++
+		}
+	}
+
+	return strconv.FormatInt(p1, 10), strconv.FormatInt(p2, 10), nil
+}
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"unicode/utf8"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var dayArg uint8
+	var pathArg string
+
+	cmd := &cobra.Command{
+		Use:          "adventofcode2025",
+		Short:        "Advent of Code - 2025 solver",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			var problem Day
+			switch dayArg {
+			case 1:
+				problem = &Day1{}
+			default:
+				return fmt.Errorf("no solution available for day %d", dayArg)
+			}
+
+			bytes, err := os.ReadFile(pathArg)
+			if err != nil {
+				return fmt.Errorf("failed to read file: %w", err)
+			}
+
+			if !utf8.Valid(bytes) {
+				return fmt.Errorf("file %s is not utf8", pathArg)
+			}
+
+			if err := problem.Parse(string(bytes)); err != nil {
+				return err
+			}
+
+			p1, p2, err := problem.Solve()
+			if err != nil {
+				return fmt.Errorf("failed to solve day %d: %w", dayArg, err)
+			}
+
+			fmt.Printf("Part 1: %s\nPart 2: %s\n", p1, p2)
+
+			return nil
+		},
+		SilenceErrors: true,
+	}
+
+	cmd.Flags().Uint8VarP(&dayArg, "day", "d", 0, "day to solve")
+	cmd.MarkFlagRequired("day")
+	cmd.Flags().StringVarP(&pathArg, "path", "p", "", "path to input file")
+	cmd.MarkFlagRequired("path")
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Printf("%s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+type Day interface {
+	Parse(string) error
+	Solve() (string, string, error)
+}
